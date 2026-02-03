1048b2d58d3cd0ffb1b337f611da9f4a4d579c17
eenzofelipe67527-star <eenzofelipe67527@gmail.com>
2026-02-03 16:41:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function contate(){
-    
+function contate() {
+  window.open("https://wa.me/55***********?text=Olá vim pelo site");
+}
+
+function promotion() {
+  window.open("https://wa.me/55***********?text=Quero saber da promoção");
 }
